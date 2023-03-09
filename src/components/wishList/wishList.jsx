85ea0e683ad3a964c104ff5e6be864efffcb5640
MilanAarick <93627593+MilanAarick@users.noreply.github.com>
2023-03-09 14:50:54 +0100

    wishList update

--- src/components/wishList/wishList.jsx
+++ src/components/wishList/wishList.jsx
@@ -3,8 +3,8 @@ import styles from './wishList.module.css'
 const WishList = () => {
     return (
         <div className={styles.bottomCard}>
-            <h4>Be a part of the Project! Sign up to the Whitelist now and secure your spot for $LEUM token at earlybird prices during presale!</h4>
-            <button className={styles.joinBtn}>Join Whitelist</button>
+            <h4>Be a part of the Project! Pre-register now and secure your spot for Dyleum token at earlybird prices during presale and other rewards!</h4>
+            <button className={styles.joinBtn}>Pre-register</button>
         </div>
     )
 }
